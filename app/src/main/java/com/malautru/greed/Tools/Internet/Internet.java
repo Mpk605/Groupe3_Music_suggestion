ea4887433271baeb26267c30d2a252962f2a60f0
Mpk605 <jules.comb@gmail.com>
2022-02-08 22:15:02 +0100

    Update Internet.java

--- app/src/main/java/com/malautru/greed/Tools/Internet/Internet.java
+++ app/src/main/java/com/malautru/greed/Tools/Internet/Internet.java
@@ -4,17 +4,15 @@ public class Internet {
 
     // Pings 8.8.8.8 and returns true if reached
     public static boolean isOnline() {
-        return true; // Slow internet
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
 
-//        Runtime runtime = Runtime.getRuntime();
-//        try {
-//            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
-//            int exitValue = ipProcess.waitFor();
-//            return (exitValue == 0);
-//        } catch (IOException | InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return false;
+        return false;
     }
 }
